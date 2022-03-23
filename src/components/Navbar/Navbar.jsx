0c920d43809f7fa6d061230d09e2d5b325964aa8
Isaac Byron <i.byron@hotmail.com>
2022-03-22 20:07:26 -0700

    Add Mobile View For Header & Banner Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import './Navbar.scss'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <>
+      <section className='nav'>
+        <div className='nav__tablet-wrapper'>
+          <h2 className='nav__header'>SP</h2>
+
+          <p className='nav__link'>HOME</p>
+          <p className='nav__link'>COMPONENTS</p>
+          <p className='nav__link'>LOGIN</p>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Navbar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Banner from '../../components/Banner/Banner'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  )
 }
 
 export default Home
